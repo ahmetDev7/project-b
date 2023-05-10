5b4a94d0d806c006e5e9382927867e457fb8f27f
Ahmet <ahmetmuratdagi7@hotmail.com>
2023-05-10 19:37:09 +0200

    Show all reservations as an admin

--- DataModels/Reservations/ReservationListModel.cs
+++ DataModels/Reservations/ReservationListModel.cs
@@ -1,6 +1,6 @@
 public static class ReservationList
 {
-    private static List<Reservation> _reservations = JsonDataAccessor<Reservation>.LoadData("DataSources/Reservations.json") ?? new List<Reservation>();
+    public static List<Reservation> _reservations = JsonDataAccessor<Reservation>.LoadData("DataSources/Reservations.json") ?? new List<Reservation>();
     // overloaded constructor where it takes no arguments and loads the json to the class's reservation list.
     // Adds the reservation to the list and writes it to the json file.
     public static void AddReservation(Reservation reservation)
@@ -13,5 +13,15 @@ public static class ReservationList
     {
         _reservations.Remove(reservation);
         JsonDataAccessor<Reservation>.WriteData("DataSources/Reservations.json", _reservations);
+    }
+
+    public static void DeleteReservationByTableNumber(int tableNumber)
+    {
+        var reservation = _reservations.FirstOrDefault(r => r.TableNumber == tableNumber);
+        if (reservation != null)
+        {
+            _reservations.Remove(reservation);
+            JsonDataAccessor<Reservation>.WriteData("DataSources/Reservations.json", _reservations);
+        }
     }
 }
