ac5ff291f54726f4c10e90d7d23c5b79f63f34f2
ahmetmuratdagi7 <ahmetmuratdagi7@hotmail.com>
2023-05-23 22:07:22 +0200

    updated customermodel

--- DataModels/Users/CustomerModel.cs
+++ DataModels/Users/CustomerModel.cs
@@ -1,13 +1,5 @@
 class Customer : User
 {
     public Customer(string userName, string passWord, string mail, string role) : base(userName, passWord, mail, role) {}
-    public void ViewReservation()
-    {
-
-    }
-    public void MakeReservation()
-    {
-        
-    }
 
 }
