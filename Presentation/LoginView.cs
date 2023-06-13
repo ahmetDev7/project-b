09520a7fccffb1ea28bc791a840b69938cbcdddb
Jurwin <43131488+JurDev@users.noreply.github.com>
2023-06-13 02:58:35 +0200

    Password now masked when login

--- Presentation/LoginView.cs
+++ Presentation/LoginView.cs
@@ -1,22 +1,56 @@
 public class LoginView
 {
     public void viewLogin()
+{   
+    User? foundAccount = null;
+    while (true)
     {   
-        User? foundAccount = null;
-        while(true){   
-            System.Console.Write("Enter your username: ");
-            string userName = Console.ReadLine()!;
-            System.Console.Write("Enter your password: ");
-            string password = Console.ReadLine()!;
-            foundAccount = AccountManager.users.FirstOrDefault(user => user.UserName == userName && user.PassWord == password);
-            if (foundAccount == null)
+        Console.Write("Enter your username: ");
+        string userName = Console.ReadLine()!;
+        
+        Console.Write("Enter your password: ");
+        string password = MaskPasswordInput();
+
+        foundAccount = AccountManager.users.FirstOrDefault(user => user.UserName == userName && user.PassWord == password);
+        
+        if (foundAccount == null)
+        {
+            Console.WriteLine("Invalid username or password, please try again.");
+        }
+        else
+        {
+            Ultilities.roleManager.Login(foundAccount.Role, foundAccount.UserId);
+            break;
+        }
+    }
+    Console.Clear();
+}
+
+private string MaskPasswordInput()
+{
+    string password = "";
+    ConsoleKeyInfo key;
+
+    do
+    {
+        key = Console.ReadKey(true);
+
+        if (key.Key == ConsoleKey.Backspace)
+        {
+            if (password.Length > 0)
             {
-                Console.WriteLine("Invalid username or password, please try again.");
-            } else{
-                Ultilities.roleManager.Login(foundAccount.Role, foundAccount.UserId);
-                break;
+                password = password.Remove(password.Length - 1);
+                Console.Write("\b \b"); // Move cursor back, overwrite character with space, move cursor back again
             }
         }
-        Console.Clear();
-    }
+        else if (key.Key != ConsoleKey.Enter)
+        {
+            password += key.KeyChar;
+            Console.Write("*");
+        }
+    } while (key.Key != ConsoleKey.Enter);
+
+    Console.WriteLine();
+    return password;
+}
 }
